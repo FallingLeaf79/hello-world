cdf5daf1113b306999503ca21f6cf5869732b473
FallingLeaf79 <44774181+FallingLeaf79@users.noreply.github.com>
2019-02-16 14:50:36 +0100

    Added BabyShark.java Doo doo doo doo doo doo doo!

---
+++ BabyShark.java
@@ -0,0 +1,22 @@
+public class BabyShark {
+  private static String[] SHARK = new String[] {
+    "Baby", "Mommy", "Daddy", "Grandma", "Grandpa"
+  };
+
+  public static void doo() {
+    for (int i = 0; i < 6; i++){
+      System.out.print("doo ");
+    }
+  }
+
+  public static void main(String[] args) {
+    for (String shark : SHARK) {
+      for (int i = 0; i < 4; i++) {
+        System.out.printf("%s shark ", shark);
+        doo();
+        System.out.println();
+      }
+      System.out.printf("%s shark!\n\n", shark);
+    }
+  }
+}
