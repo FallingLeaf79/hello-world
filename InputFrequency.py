a2d781e9a0bb847c2240d1013ec81e0d61982ade
FallingLeaf79 <44774181+FallingLeaf79@users.noreply.github.com>
2019-03-06 22:51:34 +0100

    Less redundancy

--- InputFrequency.py
+++ InputFrequency.py
@@ -1,17 +1,16 @@
 def main():
     dict = {}
-    input_string = input()
 
     while True:
-        if input_string not in dict:
-            dict[input_string] = 1
-        else:
-            dict[input_string] += 1
-
         try:
             input_string = input()
         except EOFError:
             break
+
+        if input_string not in dict:
+            dict[input_string] = 1
+        else:
+            dict[input_string] += 1
 
     for i in iter(dict):
         print("Input '{}' appears {} time(s)".format(i, dict[i]))
