6970b3dfc0f8673bef8ae4f2bd65fe2934366052
FallingLeaf79 <44774181+FallingLeaf79@users.noreply.github.com>
2019-08-21 20:17:10 +0200

    Less indentation! Inverted the first 'if' and made it throw the exception instead of executing the program. People are dumb sometimes. By people I mean myself.

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,22 +3,23 @@ public class FizzBuzz {
         String[] words = {"Fizz", "Buzz"};
         int[] multiples = {3, 5};
 
-        if (words.length == multiples.length) {
-            for (int i = 1; i <= 1000; i++) {
-                StringBuilder stringBuilder = new StringBuilder();
-
-                for (int j = 0; j < multiples.length; j++) {
-                    if (i % multiples[j] == 0) {
-                        stringBuilder.append(words[j]);
-                    }
-                }
-                if (stringBuilder.length() == 0) {
-                    stringBuilder.append(i);
-                }
-                System.out.println(stringBuilder);
-            }
-        } else {
+        if (words.length != multiples.length) {
             throw new Exception("Something went wrong there, pardner.");
+        }
+        
+        for (int i = 1; i <= 1000; i++) {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int j = 0; j < multiples.length; j++) {
+                if (i % multiples[j] == 0) {
+                    stringBuilder.append(words[j]);
+                }
+            }
+            if (stringBuilder.length() == 0) {
+                stringBuilder.append(i);
+            }
+            System.out.println(stringBuilder);
+            }
         }
     }
 }
