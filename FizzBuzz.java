89687f431029824bdb9b7fb483d576139ba12816
FallingLeaf79 <44774181+FallingLeaf79@users.noreply.github.com>
2019-08-21 20:34:52 +0200

    Comments! Comments.

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,8 +1,15 @@
+/* I attempted to take on a well-known game of FizzBuzz by making a program that
+ * has a high level of modularity (you can easily add or change words the
+ * program will print and at what multiples should that happen).
+ * Try it out for yourself; add some extra words and divisors on lines 10 and 11
+ * respectively and see what happens!
+ */
+
 public class FizzBuzz {
     public static void main(String[] args) throws Exception {
         String[] words = {"Fizz", "Buzz"};
         int[] multiples = {3, 5};
-        int limit = 100;
+        int limit = 100; //The program will print up to this number (inclusive).
 
         if (words.length != multiples.length) {
             throw new Exception("Something went wrong there, pardner.");
