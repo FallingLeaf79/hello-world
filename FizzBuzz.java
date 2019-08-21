831a4738210900bd2f270c7071d57fb6a393e7a0
FallingLeaf79 <44774181+FallingLeaf79@users.noreply.github.com>
2019-08-21 20:31:30 +0200

    Everything in one place The things that you can change for yourself are all next to each other now! c:

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,12 +2,13 @@ public class FizzBuzz {
     public static void main(String[] args) throws Exception {
         String[] words = {"Fizz", "Buzz"};
         int[] multiples = {3, 5};
+        int limit = 100;
 
         if (words.length != multiples.length) {
             throw new Exception("Something went wrong there, pardner.");
         }
 
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= limit; i++) {
             StringBuilder stringBuilder = new StringBuilder();
 
             for (int j = 0; j < multiples.length; j++) {
